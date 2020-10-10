34c3a6cf6f9f2c4d2e2278440a7362271b555ae2
Wind2009-Louse <wind.jackey@qq.com>
2020-10-10 16:26:29 +0800

    修复不支持异步的问题

--- src/main/java/com/weird/BootRunner.java
+++ src/main/java/com/weird/BootRunner.java
@@ -3,10 +3,12 @@ package com.weird;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class BootRunner extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BootRunner.class, args);
