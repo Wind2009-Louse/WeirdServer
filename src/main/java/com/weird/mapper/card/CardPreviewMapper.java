94e385fe298e817db92041ce5ff960db690bfdb1
Wind2009-Louse <wind.jackey@qq.com>
2021-07-16 22:43:22 +0800

    添加卡查搜索条件

--- src/main/java/com/weird/mapper/card/CardPreviewMapper.java
+++ src/main/java/com/weird/mapper/card/CardPreviewMapper.java
@@ -1,9 +1,13 @@
 package com.weird.mapper.card;
 
 import com.weird.model.CardPreviewModel;
+import com.weird.model.enums.CardAttributeEnum;
+import com.weird.model.enums.CardRaceEnum;
+import com.weird.model.enums.CardTypeEnum;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 卡片描述查询Mapper
@@ -27,4 +31,20 @@ public interface CardPreviewMapper {
      * @return 卡名列表
      */
     List<String> blurSearch(@Param("word") String word);
+
+    /**
+     * 多重条件查询
+     *
+     * @param wordList 卡名/效果列表
+     * @param typeList 类型列表
+     * @return
+     */
+    List<String> multiBlurSearch(@Param("wordList") List<String> wordList,
+                                 @Param("typeList") List<CardTypeEnum> typeList,
+                                 @Param("attributeList") List<CardAttributeEnum> attributeList,
+                                 @Param("raceList") List<CardRaceEnum> raceList,
+                                 @Param("level") Long level,
+                                 @Param("attack") Long attack,
+                                 @Param("defense") Long defense,
+                                 @Param("scale") Long scale);
 }
--- src/main/java/com/weird/service/impl/CardPreviewServiceImpl.java
+++ src/main/java/com/weird/service/impl/CardPreviewServiceImpl.java
@@ -2,6 +2,9 @@ package com.weird.service.impl;
 
 import com.weird.mapper.card.CardPreviewMapper;
 import com.weird.model.CardPreviewModel;
+import com.weird.model.enums.CardAttributeEnum;
+import com.weird.model.enums.CardRaceEnum;
+import com.weird.model.enums.CardTypeEnum;
 import com.weird.service.CardPreviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 卡片详细Service
@@ -23,6 +25,14 @@ import java.util.List;
 public class CardPreviewServiceImpl implements CardPreviewService {
     @Autowired
     CardPreviewMapper cardPreviewMapper;
+
+    static String[] cardTypeFilter = {"t:", "type:", "t=", "type="};
+    static String[] cardAttributeFilter = {"a:", "attribute:", "a=", "attribute="};
+    static String[] cardRaceFilter = {"r:", "race:", "r=", "race="};
+    static String[] cardLevelFilter = {"l:", "lv:", "level:", "l=", "lv=", "level="};
+    static String[] cardAttackFilter = {"atk:", "attack:", "atk=", "attack="};
+    static String[] cardDefenseFilter = {"def:", "defense:", "def=", "defense="};
+    static String[] cardScaleFilter = {"ls:", "scale:", "ls=", "scale="};
 
     /**
      * 根据卡名返回卡片详情
@@ -56,6 +66,125 @@ public class CardPreviewServiceImpl implements CardPreviewService {
         if (StringUtils.isEmpty(word)) {
             return null;
         }
-        return cardPreviewMapper.blurSearch(word);
+        List<String> cardDescList = new LinkedList<>();
+        Set<CardTypeEnum> cardTypeSet = new HashSet<>();
+        Set<CardAttributeEnum> cardAttributeSet = new HashSet<>();
+        Set<CardRaceEnum> cardRaceSet = new HashSet<>();
+        Long cardLevel = null;
+        Long cardAttack = null;
+        Long cardDefense = null;
+        Long cardScale = null;
+
+        for (String rawText : word.split("\\|")) {
+            boolean filtered = false;
+            String realText = rawText.trim();
+            if (StringUtils.isEmpty(realText)) {
+                continue;
+            }
+
+            for (String filter : cardTypeFilter) {
+                if (realText.startsWith(filter)) {
+                    String filteredText = realText.substring(filter.length());
+                    CardTypeEnum cardType = CardTypeEnum.getByName(filteredText);
+                    if (cardType != null) {
+                        cardTypeSet.add(cardType);
+                        filtered = true;
+                    }
+                }
+            }
+
+            for (String filter : cardAttributeFilter) {
+                if (realText.startsWith(filter)) {
+                    String filteredText = realText.substring(filter.length());
+                    CardAttributeEnum cardAttribute = CardAttributeEnum.getByName(filteredText);
+                    if (cardAttribute != null) {
+                        cardAttributeSet.add(cardAttribute);
+                        filtered = true;
+                    }
+                }
+            }
+
+            for (String filter : cardRaceFilter) {
+                if (realText.startsWith(filter)) {
+                    String filteredText = realText.substring(filter.length());
+                    CardRaceEnum cardRace = CardRaceEnum.getByName(filteredText);
+                    if (cardRace != null) {
+                        cardRaceSet.add(cardRace);
+                        filtered = true;
+                    }
+                }
+            }
+
+            for (String filter : cardLevelFilter) {
+                if (realText.startsWith(filter)) {
+                    String filteredText = realText.substring(filter.length());
+                    try {
+                        cardLevel = Long.parseLong(filteredText);
+                        filtered = true;
+                        break;
+                    } catch (Exception e) {
+                        cardLevel = null;
+                    }
+                }
+            }
+
+            for (String filter : cardAttackFilter) {
+                if (realText.startsWith(filter)) {
+                    String filteredText = realText.substring(filter.length());
+                    try {
+                        cardAttack = Long.parseLong(filteredText);
+                        filtered = true;
+                        break;
+                    } catch (Exception e) {
+                        cardAttack = null;
+                    }
+                }
+            }
+
+            for (String filter : cardDefenseFilter) {
+                if (realText.startsWith(filter)) {
+                    String filteredText = realText.substring(filter.length());
+                    try {
+                        cardDefense = Long.parseLong(filteredText);
+                        filtered = true;
+                        break;
+                    } catch (Exception e) {
+                        cardDefense = null;
+                    }
+                }
+            }
+
+            for (String filter : cardScaleFilter) {
+                if (realText.startsWith(filter)) {
+                    String filteredText = realText.substring(filter.length());
+                    try {
+                        cardScale = Long.parseLong(filteredText);
+                        filtered = true;
+                        break;
+                    } catch (Exception e) {
+                        cardScale = null;
+                    }
+                }
+            }
+
+            if (!filtered) {
+                cardDescList.add(realText);
+            }
+        }
+
+        List<CardTypeEnum> cardTypeList = new LinkedList<>(cardTypeSet);
+        List<CardAttributeEnum> cardAttributeList = new LinkedList<>(cardAttributeSet);
+        List<CardRaceEnum> cardRaceList = new LinkedList<>(cardRaceSet);
+
+        // TODO 刻度搜索
+        return cardPreviewMapper.multiBlurSearch(
+                cardDescList,
+                cardTypeList,
+                cardAttributeList,
+                cardRaceList,
+                cardLevel,
+                cardAttack,
+                cardDefense,
+                cardScale);
     }
 }
