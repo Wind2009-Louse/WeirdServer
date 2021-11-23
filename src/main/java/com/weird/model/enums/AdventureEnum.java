861ad3537e8d380d06d76167052dce1f3185b5d8
Wind2009-Louse <wind.jackey@qq.com>
2021-11-23 17:25:57 +0800

    更新冒险信息

--- src/main/java/com/weird/model/enums/AdventureEnum.java
+++ src/main/java/com/weird/model/enums/AdventureEnum.java
@@ -16,8 +16,8 @@ public enum AdventureEnum {
      */
     STARTER(0, "起点", "起点", SCORPION),
     SECRET_WAY(1, "通向财宝的隐藏通路", "隐藏通路|隐藏通道|左腕的代偿|左腕的代价|王家的财宝|欲望的幻象", "拥有多种可能性的通路！"),
-    ZERA(2, "杰拉", "魔杰拉|暗杰拉|黑杰拉", NORMAL_DESC + FIELD_CROSS_WAY),
-    LIGHT_ZERA(3, "天空的使者 杰拉迪亚斯", "光杰拉|代行", NORMAL_DESC + FIELD_CROSS_WAY),
+    ZERA(2, "大魔王 魔杰拉", "魔杰拉|暗杰拉|黑杰拉", NORMAL_DESC + FIELD_CROSS_WAY),
+    LIGHT_ZERA(3, "大天使 杰拉特", "光杰拉|代行|天空的使者|杰拉迪亚斯", NORMAL_DESC + FIELD_CROSS_WAY),
     LIGHTLORD(4, "光道少女 密涅瓦", "光道少女|米涅瓦", NORMAL_DESC + FIELD_CROSS_WAY),
     DARKWORLD(5, "暗黑界的导师 塞鲁利", "暗黑界", NORMAL_DESC + FIELD_CROSS_WAY),
 
@@ -26,14 +26,14 @@ public enum AdventureEnum {
     SCIENTIST(8, "魔导科学家", "魔科", "魔导科学家会带来怎样的科技呢……"),
     EXODIA(9, "守护神 艾克佐德", "艾克佐迪亚|老I|老艾|岩石|魔救", NORMAL_DESC + FIELD_WASTELAND),
     SPARTA(10, "斯巴达咒术师", "剑斗兽", NORMAL_DESC + FIELD_WASTELAND),
-    CHAOS(11, "混沌的使者", "混沌使者|混沌战士|混沌战士", NORMAL_DESC + FIELD_WASTELAND),
+    CHAOS(11, "圣女 贞德", "圣女贞德|救祓少女", NORMAL_DESC + FIELD_WASTELAND),
 
     HALF_SCORPION(12, "黑蝎盗掘团", "黑蝎", SCORPION),
-    GRAVE_KEEPER(13, "守墓的异端者", "守墓的异能者", NORMAL_DESC + FIELD_GRAVE),
+    GRAVE_KEEPER(13, "鬼计妖魔 阿鲁卡德", "A少", "不给糖就捣鬼计！"),
     SKULL(14, "白骨王子", "白骨", NORMAL_DESC + FIELD_GRAVE),
-    FANDORA(15, "飞龙艇-幻舵拉", "空牙|飞船|飞艇|飞翼犀牛|外星人母后|死域海的灯塔|玄化|Boss|boss|BOSS", "飞龙艇的目的地是——" + FIELD_CELESTIA),
-    KOZAKY(16, "平庸鬼", "平庸鬼|自爆", "看，平庸鬼！" + FIELD_GRAVE),
-    SHIP(17, "灵魂护送船", "灵魂船|幽灵船|传说的渔人|海皇子 尼普深渊王|熔岩裁决王|炎王兽 甘尼许|忘却的海底神殿", "一艘被恶灵占领的破船……" + FIELD_SEA),
+    FANDORA(15, "太空翻车鱼", "翻车鱼|空牙|飞船|飞艇|飞翼犀牛|外星人母后|死域海的灯塔|玄化|Boss|boss|BOSS|碑像天使|亚兹卢恩死月弓碑|太空旋风", "神秘飞船的目的地是——" + FIELD_CELESTIA),
+    KOZAKY(16, "平庸鬼", "自爆", "看，平庸鬼！" + FIELD_GRAVE),
+    SHIP(17, "灵魂护送船", "灵魂船|幽灵船|传说的渔人|海皇子 尼普深渊王|熔岩裁决王|炎王兽 甘尼许|忘却的海底神殿|舍利军贯|军舰", "一艘被恶灵占领的破船……" + FIELD_SEA),
 
     RED_EYE(18, "正义的同伴 城之内侠", "正义的同伴 成之内侠|真红眼|凡骨", NORMAL_DESC + FIELD_JUSTICE),
     GOBLIN(19, "哥布林王", "哥布林王", "看上去是彬彬有礼那种？"),
